93c6fe838226691532185f2eb1c5ff8512bdc304
Tim Turner <timdturner@gmail.com>
2018-04-21 19:44:26 -0400

    Add intial prototype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import aframe from 'aframe';
+import starsystem from 'aframe-star-system-component';
+import outline from 'aframe-outline';
+import extras from 'aframe-extras';
 
-console.log('test');
+aframe.registerComponent('cursor-listener', {
+    init: function() {
+        console.log('init');
+        //var rock = document.querySelector('#rock');
+        this.el.addEventListener('mouseenter', function(evt) {
+            this.setAttribute('color', 'blue');
+            console.log(evt.detail, this);
+        });
+
+        this.el.addEventListener('click', function(evt) {
+            var music = document.querySelector('[sound]');
+            music.components.sound.playSound();
+        });
+    }
+});
